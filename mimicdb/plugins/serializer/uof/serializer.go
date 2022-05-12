eeec9ef6f4e166bff3e345123a97fc70fb56495b
Satoshi Konno <skonno@cybergarage.org>
2022-05-13 00:48:15 +0900

    Add uof.NewSerializer()

---
+++ mimicdb/plugins/serializer/uof/serializer.go
@@ -0,0 +1,40 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package uof
+
+import (
+	"github.com/cybergarage/mimicdb/mimicdb/obj"
+	"github.com/cybergarage/mimicdb/mimicdb/plugins/serializer"
+)
+
+// Memdb represents a Memdb instance.
+type UOF struct {
+	serializer.Serializer
+}
+
+// NewSerializer returns a new serializer instance.
+func NewSerializer() *UOF {
+	return &UOF{}
+}
+
+// Encode dumps a specified object to the byte array.
+func (s *UOF) Encode(obj *obj.Object) ([]byte, error) {
+	return make([]byte, 0), nil
+}
+
+// Decode creates an object from the specified byte array.
+func (s *UOF) Decode([]byte) (*obj.Object, error) {
+	return nil, nil
+}
