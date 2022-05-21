cfa8168b4c3fa36815b88cb21b5d2c650fc4fe7f
Satoshi Konno <skonno@cybergarage.org>
2022-05-21 19:34:34 +0900

    Add object types

---
+++ mimicdb/obj/type.go
@@ -0,0 +1,40 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package obj
+
+type Type uint8
+
+const (
+	DICTIONARY = Type(0x01)
+	ARRAY      = Type(0x02)
+	NAME       = Type(0x10)
+	STRING     = Type(0x11)
+	TINY       = Type(0x20)
+	SHORT      = Type(0x21)
+	INT        = Type(0x22)
+	LONG       = Type(0x23)
+	VARINT     = Type(0x25)
+	COUNTER    = Type(0x24)
+	FLOAT      = Type(0x30)
+	DOUBLE     = Type(0x31)
+	DECIMAL    = Type(0x32)
+	TIMESTAMP  = Type(0x40)
+	DATETIME   = Type(0x41)
+	NULL       = Type(0x80)
+	BOOL       = Type(0x81)
+	BINARY     = Type(0x82)
+	UUID       = Type(0x83)
+	INET       = Type(0x84)
+)
