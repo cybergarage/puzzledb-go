08eeca6c58617be99dc8a2a34b70ad11dac46a11
Satoshi Konno <skonno@cybergarage.org>
2022-11-01 00:02:59 +0900

    Add Executor

--- mimicdb/executor.go
+++ mimicdb/executor.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package executor
+package mimicdb
 
 // Executor represents a statement executor.
 type Executor interface {
