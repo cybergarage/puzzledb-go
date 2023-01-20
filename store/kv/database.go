d931503f068acdbabdbcf143639c6be799b50cd6
Satoshi Konno <skonno@cybergarage.org>
2023-01-20 19:40:29 +0900

    Update package names

--- store/kv/database.go
+++ store/kv/database.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package store
+package kv
 
 // Database represents a database interface.
 type Database interface {
