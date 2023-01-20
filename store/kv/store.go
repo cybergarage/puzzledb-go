f02113cf2cb47cdd40c59952b204a1cf4dd19c68
Satoshi Konno <skonno@cybergarage.org>
2023-01-20 19:40:29 +0900

    Update package names

--- store/kv/store.go
+++ store/kv/store.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package store
+package kv
 
 // Store represents a store interface.
 type Store interface {
