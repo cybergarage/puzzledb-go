f6ccf5c13b522fb9d0f51cb1446ad4d6f43bf7da
Satoshi Konno <skonno@cybergarage.org>
2023-01-21 23:46:27 +0900

    Update store.transaction

---
+++ server/plugins/store/transaction.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package store
+
+import (
+	"github.com/cybergarage/puzzledb-go/puzzledb/document"
+	"github.com/cybergarage/puzzledb-go/puzzledb/store"
+	"github.com/cybergarage/puzzledb-go/puzzledb/store/kv"
+)
+
+type transaction struct {
+	kv kv.Transaction
+	document.Serializer
+}
+
+// Insert puts a key-value object.
+func (txn *transaction) Insert(obj *store.Object) error {
+	return nil
+}
+
+// Select gets an key-value object of the specified key.
+func (txn *transaction) Select(key store.Key) (*store.Object, error) {
+	return nil, nil
+}
+
+// Commit commits this transaction.
+func (txn *transaction) Commit() error {
+	return txn.kv.Commit()
+}
+
+// Cancel cancels this transaction.
+func (txn *transaction) Cancel() error {
+	return txn.kv.Cancel()
+}
