1d8d1425e76a9125c05dc215052d9fcb1c9b3294
Satoshi Konno <skonno@cybergarage.org>
2023-01-27 00:03:58 +0900

    Update Index::AddElement()

--- server/plugins/store/index.go
+++ server/plugins/store/index.go
@@ -20,27 +20,28 @@ import (
 
 // Schema format (version 1)
 //
-// 0: uint8 - version
-// 1: string - name
-// 2: colums - map[int8]any
-//
-// colums
-//
+// 1: name - string
+// 2: type - uint8
+// 3: elements - []string
 
 const (
-	indexNameIdx = 1
-	indexTypeIdx = 2
+	indexNameIdx     = 1
+	indexTypeIdx     = 2
+	indexElementsIdx = 3
 )
 
 type index struct {
-	data map[uint8]any
+	data     map[uint8]any
+	elements []store.Element
 }
 
 // NewIndex returns a blank index.
 func NewIndex() *index {
 	idx := &index{
-		data: map[uint8]any{},
+		data:     map[uint8]any{},
+		elements: []store.Element{},
 	}
+	idx.data[indexElementsIdx] = []string{}
 	return idx
 }
 
@@ -84,7 +85,21 @@ func (idx *index) Type() store.IndexType {
 	}
 }
 
+// AddElement returns the schema elements.
+func (idx *index) AddElement(elem store.Element) {
+	idx.elements = append(idx.elements, elem)
+	v, ok := idx.data[indexElementsIdx]
+	if !ok {
+		return
+	}
+	a, ok := v.([]string)
+	if !ok {
+		return
+	}
+	idx.data[indexElementsIdx] = append(a, elem.Name())
+}
+
 // Elements returns the schema elements.
 func (idx *index) Elements() []store.Element {
-	return []store.Element{}
+	return idx.elements
 }
