d1fe88d19497f408f048837e0e0c31ce821d254a
Satoshi Konno <skonno@cybergarage.org>
2023-02-19 08:53:44 +0900

    Update BSONDecoder

--- server/plugins/query/mongo/service.go
+++ server/plugins/query/mongo/service.go
@@ -34,7 +34,7 @@ func NewService() *Service {
 		Server:      mongo.NewServer(),
 		BaseService: query.NewService(),
 		documents:   make([]bson.Document, 0),
-		Serializer: NewSerializer(),
+		Serializer:  NewSerializer(),
 	}
 
 	server.SetMessageListener(server)
