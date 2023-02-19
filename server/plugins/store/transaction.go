99e754f01ed06fd7c72b3e1b9e9687627faad105
Satoshi Konno <skonno@cybergarage.org>
2023-02-20 00:53:00 +0900

    Add store.Transaction::RemoveIndex()

--- server/plugins/store/transaction.go
+++ server/plugins/store/transaction.go
@@ -28,23 +28,23 @@ type transaction struct {
 }
 
 // InsertDocument puts a document object with the primary key.
-func (txn *transaction) InsertDocument(key store.Key, obj store.Object) error {
+func (txn *transaction) InsertDocument(docKey store.Key, obj store.Object) error {
 	var encObj bytes.Buffer
 	err := txn.Encode(&encObj, obj)
 	if err != nil {
 		return err
 	}
-	docKey := kv.NewKeyWith(kv.DocumentKeyHeader, key)
+	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
 	kvObj := kv.Object{
-		Key:   docKey,
+		Key:   kvDocKey,
 		Value: encObj.Bytes(),
 	}
 	return txn.kv.Set(&kvObj)
 }
 
 // FindDocuments gets document objects matching the specified key.
-func (txn *transaction) FindDocuments(key store.Key) ([]store.Object, error) {
-	kvObjs, err := txn.kv.Get(kv.NewKeyWith(kv.DocumentKeyHeader, key))
+func (txn *transaction) FindDocuments(docKey store.Key) ([]store.Object, error) {
+	kvObjs, err := txn.kv.Get(kv.NewKeyWith(kv.DocumentKeyHeader, docKey))
 	if err != nil {
 		return nil, err
 	}
@@ -60,35 +60,41 @@ func (txn *transaction) FindDocuments(key store.Key) ([]store.Object, error) {
 }
 
 // RemoveDocument removes a document object with the specified primary key.
-func (txn *transaction) RemoveDocument(key store.Key) error {
-	docKey := kv.NewKeyWith(kv.DocumentKeyHeader, key)
-	return txn.kv.Remove(docKey)
+func (txn *transaction) RemoveDocument(docKey store.Key) error {
+	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
+	return txn.kv.Remove(kvDocKey)
 }
 
-// InsertDocument puts a document object with the specified primary key.
-func (txn *transaction) InsertIndex(key store.Key, prKey store.Key) error {
-	docKey := kv.NewKeyWith(kv.DocumentKeyHeader, prKey)
-	docKeyBytes, err := docKey.Encode()
+// InsertIndex puts a secondary index with the primary key.
+func (txn *transaction) InsertIndex(idxKey store.Key, docKey store.Key) error {
+	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
+	kvDocKeyBytes, err := kvDocKey.Encode()
 	if err != nil {
 		return err
 	}
 	var encDocKey bytes.Buffer
-	err = txn.Encode(&encDocKey, docKeyBytes)
+	err = txn.Encode(&encDocKey, kvDocKeyBytes)
 	if err != nil {
 		return err
 	}
-	idxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, key)
+	kvIdxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, idxKey)
 	kvObj := kv.Object{
-		Key:   idxKey,
+		Key:   kvIdxKey,
 		Value: encDocKey.Bytes(),
 	}
 	return txn.kv.Set(&kvObj)
 }
 
+// RemoveIndex removes the specified secondary index.
+func (txn *transaction) RemoveIndex(idxKey store.Key) error {
+	kvIdxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, idxKey)
+	return txn.kv.Remove(kvIdxKey)
+}
+
 // FindDocumentsByIndex gets document objects matching the specified index key.
-func (txn *transaction) FindDocumentsByIndex(indexKey store.Key) ([]store.Object, error) {
-	idxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, indexKey)
-	kvIdxObjs, err := txn.kv.Get(idxKey)
+func (txn *transaction) FindDocumentsByIndex(idxKey store.Key) ([]store.Object, error) {
+	kvIdxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, idxKey)
+	kvIdxObjs, err := txn.kv.Get(kvIdxKey)
 	if err != nil {
 		return nil, err
 	}
@@ -114,15 +120,15 @@ func (txn *transaction) FindDocumentsByIndex(indexKey store.Key) ([]store.Object
 }
 
 // UpdateDocument updates a document object with the specified primary key.
-func (txn *transaction) UpdateDocument(key store.Key, obj store.Object) error {
+func (txn *transaction) UpdateDocument(docKey store.Key, obj store.Object) error {
 	var encObj bytes.Buffer
 	err := txn.Encode(&encObj, obj)
 	if err != nil {
 		return err
 	}
-	docKey := kv.NewKeyWith(kv.DocumentKeyHeader, key)
+	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
 	kvObj := kv.Object{
-		Key:   docKey,
+		Key:   kvDocKey,
 		Value: encObj.Bytes(),
 	}
 	return txn.kv.Set(&kvObj)
