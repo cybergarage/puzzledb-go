b379df365ef84e1bb6f8cfb8396021fa0e0d347b
Satoshi Konno <skonno@cybergarage.org>
2023-02-26 19:07:12 +0900

    Update store.newTransaction()

--- server/plugins/store/transaction.go
+++ server/plugins/store/transaction.go
@@ -15,8 +15,6 @@
 package store
 
 import (
-	"bytes"
-
 	"github.com/cybergarage/puzzledb-go/puzzledb/document"
 	"github.com/cybergarage/puzzledb-go/puzzledb/store"
 	"github.com/cybergarage/puzzledb-go/puzzledb/store/kv"
@@ -27,85 +25,11 @@ type transaction struct {
 	document.Serializer
 }
 
-// InsertDocument puts a document object with the primary key.
-func (txn *transaction) InsertDocument(docKey store.Key, obj store.Object) error {
-	var encObj bytes.Buffer
-	err := txn.Encode(&encObj, obj)
-	if err != nil {
-		return err
-	}
-	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
-	kvObj := kv.Object{
-		Key:   kvDocKey,
-		Value: encObj.Bytes(),
-	}
-	return txn.kv.Set(&kvObj)
-}
-
-// FindDocuments returns a result set matching the specified key.
-func (txn *transaction) FindDocuments(docKey store.Key) (store.ResultSet, error) {
-	kvRs, err := txn.kv.Get(kv.NewKeyWith(kv.DocumentKeyHeader, docKey))
-	if err != nil {
-		return nil, err
-	}
-	return newResultSet(txn.Serializer, kvRs), nil
-}
-
-// RemoveDocument removes a document object with the specified primary key.
-func (txn *transaction) RemoveDocument(docKey store.Key) error {
-	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
-	return txn.kv.Remove(kvDocKey)
-}
-
-// InsertIndex puts a secondary index with the primary key.
-func (txn *transaction) InsertIndex(idxKey store.Key, docKey store.Key) error {
-	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
-	kvDocKeyBytes, err := kvDocKey.Encode()
-	if err != nil {
-		return err
-	}
-	var encDocKey bytes.Buffer
-	err = txn.Encode(&encDocKey, kvDocKeyBytes)
-	if err != nil {
-		return err
-	}
-	kvIdxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, idxKey)
-	kvObj := kv.Object{
-		Key:   kvIdxKey,
-		Value: encDocKey.Bytes(),
-	}
-	return txn.kv.Set(&kvObj)
-}
-
-// RemoveIndex removes the specified secondary index.
-func (txn *transaction) RemoveIndex(idxKey store.Key) error {
-	kvIdxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, idxKey)
-	return txn.kv.Remove(kvIdxKey)
-}
-
-// FindDocumentsByIndex gets document objects matching the specified index key.
-func (txn *transaction) FindDocumentsByIndex(idxKey store.Key) (store.ResultSet, error) {
-	kvIdxKey := kv.NewKeyWith(kv.SecondaryIndexHeader, idxKey)
-	kvIdxRs, err := txn.kv.Get(kvIdxKey)
-	if err != nil {
-		return nil, err
-	}
-	return newIndexResultSet(txn, txn.Serializer, kvIdxRs), nil
-}
-
-// UpdateDocument updates a document object with the specified primary key.
-func (txn *transaction) UpdateDocument(docKey store.Key, obj store.Object) error {
-	var encObj bytes.Buffer
-	err := txn.Encode(&encObj, obj)
-	if err != nil {
-		return err
-	}
-	kvDocKey := kv.NewKeyWith(kv.DocumentKeyHeader, docKey)
-	kvObj := kv.Object{
-		Key:   kvDocKey,
-		Value: encObj.Bytes(),
-	}
-	return txn.kv.Set(&kvObj)
+func newTransaction(kvTx kv.Transaction, serializer document.Serializer) (store.Transaction, error) {
+	return &transaction{
+		kv:         kvTx,
+		Serializer: serializer,
+	}, nil
 }
 
 // Commit commits this transaction.
