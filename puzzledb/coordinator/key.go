0cc4a6bd6a7c78e99d0037f36f5f008897b817bb
Satoshi Konno <skonno@cybergarage.org>
2023-03-20 12:57:19 +0900

    Add coordinator.NewKey()

---
+++ puzzledb/coordinator/key.go
@@ -0,0 +1,62 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package coordinator
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Key represents an unique key for a key-value object.
+type Key []any
+
+// NewKey returns a new blank key.
+func NewKey() Key {
+	return Key{}
+}
+
+// NewKeyWith returns a new key from the specified key elements.
+func NewKeyWith(elems ...any) Key {
+	elemArray := make([]any, len(elems))
+	copy(elemArray, elems)
+	return elemArray
+}
+
+// Elements returns all elements of the key.
+func (key Key) Elements() []any {
+	return key
+}
+
+// Encode encodes the key to a byte array.
+func (key Key) Encode() ([]byte, error) {
+	var keyBuf bytes.Buffer
+	for _, elem := range key {
+		switch v := elem.(type) {
+		case string:
+			if _, err := keyBuf.WriteString(v); err != nil {
+				return nil, err
+			}
+		case []byte:
+			if _, err := keyBuf.Write(v); err != nil {
+				return nil, err
+			}
+		default:
+			if _, err := keyBuf.WriteString(fmt.Sprintf("%v", v)); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return keyBuf.Bytes(), nil
+}
