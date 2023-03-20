c7eed1acee43efe521be294dc8caee51ed0f48a7
Satoshi Konno <skonno@cybergarage.org>
2023-03-21 08:48:09 +0900

    Add coordinator.Transaction interface

---
+++ puzzledb/coordinator/transaction.go
@@ -0,0 +1,29 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package coordinator
+
+// Transaction represents a transaction interface.
+type Transaction interface {
+	// Set sets the value for the specified key.
+	Set(key Key, value Value) error
+	// Get gets the value for the specified key.
+	Get(key Key) (Value, error)
+	// Range gets the resultset for the specified key range.
+	Range(key Key) (ResultSet, error)
+	// Commit commits this transaction.
+	Commit() error
+	// Cancel cancels this transaction.
+	Cancel() error
+}
