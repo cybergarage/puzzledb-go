63b525de3da4c1abf626ac98cd79ff8f6e14863e
Satoshi Konno <skonno@cybergarage.org>
2023-04-11 12:18:11 +0900

    Update fdb.Transaction()

--- puzzledb/plugins/coordinator/core/fdb/transaction.go
+++ puzzledb/plugins/coordinator/core/fdb/transaction.go
@@ -15,43 +15,74 @@
 package fdb
 
 import (
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
 	"github.com/cybergarage/puzzledb-go/puzzledb/coordinator"
 )
 
-type Txn struct {
+// transaction represents a transaction instance.
+type transaction struct {
+	fdb.Transaction
 }
 
-// NewTransaction returns a new transaction.
-func NewTransaction() coordinator.Transaction {
-	return &Txn{}
+func newTransaction(txn fdb.Transaction) coordinator.Transaction {
+	return &transaction{
+		Transaction: txn,
+	}
 }
 
-// Set sets the object for the specified key.
-func (txn *Txn) Set(obj coordinator.Object) error {
+// Set stores a key-value object. If the key already holds some value, it is overwritten.
+func (txn *transaction) Set(obj coordinator.Object) error {
+	keyBytes, err := obj.Key().Encode()
+	if err != nil {
+		return err
+	}
+	objBytes, err := obj.Encode()
+	if err != nil {
+		return err
+	}
+	txn.Transaction.Set(fdb.Key(keyBytes), objBytes)
 	return nil
 }
 
-// Get gets the object for the specified key.
-func (txn *Txn) Get(key coordinator.Key) (coordinator.Object, error) {
-	return nil, nil
+// Get returns a key-value object of the specified key.
+func (txn *transaction) Get(key coordinator.Key) (coordinator.Object, error) {
+	keyBytes, err := key.Encode()
+	if err != nil {
+		return nil, err
+	}
+	fbs := txn.Transaction.Get(fdb.Key(keyBytes))
+	val, err := fbs.Get()
+	if err != nil {
+		return nil, err
+	}
+	// NOTE: FutureByteSlice::Get() doesn't return nil if the key doesn't exist.
+	if len(val) == 0 {
+		return nil, coordinator.NewKeyNotExistError(key)
+	}
+	return coordinator.NewObjectWith(key, val), nil
 }
 
-// Range gets the resultset for the specified key range.
-func (txn *Txn) Range(key coordinator.Key) (coordinator.ResultSet, error) {
-	return nil, nil
-}
-
-// Delete deletes the object for the specified key.
-func (txn *Txn) Delete(key coordinator.Key) error {
+// Delete removes the specified key-value object.
+func (txn *transaction) Delete(key coordinator.Key) error {
+	keyBytes, err := key.Encode()
+	if err != nil {
+		return err
+	}
+	txn.Transaction.Clear(fdb.Key(keyBytes))
 	return nil
 }
 
 // Commit commits this transaction.
-func (txn *Txn) Commit() error {
+func (txn *transaction) Commit() error {
+	err := txn.Transaction.Commit().Get()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // Cancel cancels this transaction.
-func (txn *Txn) Cancel() error {
+func (txn *transaction) Cancel() error {
+	txn.Transaction.Cancel()
 	return nil
 }
