9c7e0f4bcc1ab0980d8db172ffdead55b712a110
Satoshi Konno <skonno@cybergarage.org>
2023-04-14 01:01:22 +0900

    Update key.CoderTest()

--- puzzledbtest/plugins/coder/key/coder_helper.go
+++ puzzledbtest/plugins/coder/key/coder_helper.go
@@ -15,13 +15,19 @@
 package key
 
 import (
+	_ "embed"
+	"strconv"
+
 	"fmt"
-	"math"
 	"reflect"
 	"testing"
 
+	"github.com/cybergarage/go-pict/pict"
 	"github.com/cybergarage/puzzledb-go/puzzledb/document"
 )
+
+//go:embed key_types.pict
+var testKeyTypes []byte
 
 // nolint:goerr113
 func DeepEqual(x, y any) error {
@@ -37,18 +43,114 @@ func DeepEqual(x, y any) error {
 	return fmt.Errorf("%v != %v", x, y)
 }
 
+// nolint:goerr113, gocognit, gci
 func CoderTest(t *testing.T, coder document.KeyCoder) {
 	t.Helper()
 
-	tests := []struct {
-		name string
-		obj  any
-	}{
-		{"int", math.MaxInt},
+	pict := pict.NewParserWithBytes(testKeyTypes)
+	err := pict.Parse()
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+	getKeyValue := func(t string, v string) (any, error) {
+		switch t {
+		case "string":
+			return v, nil
+		case "int":
+			i, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			return int(i), nil
+		case "int8":
+			i, err := strconv.ParseInt(v, 10, 8)
+			if err != nil {
+				return nil, err
+			}
+			return int8(i), nil
+		case "int16":
+			i, err := strconv.ParseInt(v, 10, 16)
+			if err != nil {
+				return nil, err
+			}
+			return int16(i), nil
+		case "int32":
+			i, err := strconv.ParseInt(v, 10, 32)
+			if err != nil {
+				return nil, err
+			}
+			return int32(i), nil
+		case "int64":
+			i, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			return int64(i), nil
+		case "uint":
+			i, err := strconv.ParseUint(v, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			return uint(i), nil
+		case "uint8":
+			i, err := strconv.ParseUint(v, 10, 8)
+			if err != nil {
+				return nil, err
+			}
+			return uint8(i), nil
+		case "uint16":
+			i, err := strconv.ParseUint(v, 10, 16)
+			if err != nil {
+				return nil, err
+			}
+			return uint16(i), nil
+		case "uint32":
+			i, err := strconv.ParseUint(v, 10, 32)
+			if err != nil {
+				return nil, err
+			}
+			return uint32(i), nil
+		case "uint64":
+			i, err := strconv.ParseUint(v, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			return uint64(i), nil
+		default:
+			return nil, fmt.Errorf("unknown type: %s", t)
+		}
+	}
+
+	pictParams := pict.Params()
+	for i, pictCase := range pict.Cases() {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			key := document.NewKey()
+			for n, pictParam := range pictParams {
+				kv, err := getKeyValue(pictParam, pictCase[n])
+				if err != nil {
+					t.Error(err)
+					return
+				}
+				key = append(key, kv)
+			}
+			kb, err := coder.Encode(key)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			decKey, err := coder.Decode(kb)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			err = DeepEqual(key, decKey)
+			if err != nil {
+				t.Error(err)
+				return
+			}
 		})
 	}
 }
