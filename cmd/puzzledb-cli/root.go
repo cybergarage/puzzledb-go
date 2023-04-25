873cadeabc19521f27eff8c6b8dd75b58b815ef8
Satoshi Konno <skonno@cybergarage.org>
2023-04-25 23:57:56 +0900

    Update puzzledb-cli

--- cmd/puzzledb-cli/root.go
+++ cmd/puzzledb-cli/root.go
@@ -28,7 +28,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "puzzledb-go",
+	Use:   "puzzledb-cli",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -57,7 +57,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.puzzledb-go.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.puzzledb-cli.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
