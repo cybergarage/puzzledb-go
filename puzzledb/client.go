bc325823cdce0d7b476a4043d018489ecb7cf08c
Satoshi Konno <skonno@cybergarage.org>
2023-05-01 21:36:41 +0900

    Update Client::Open()

--- puzzledb/client.go
+++ puzzledb/client.go
@@ -14,12 +14,49 @@
 
 package puzzledb
 
+import (
+	"net"
+	"strconv"
+
+	"google.golang.org/grpc"
+)
+
+// Client represens a gRPC client.
 type Client struct {
+	Host string
+	Port int
+	Conn *grpc.ClientConn
 }
 
+// NewClient returns a new gRPC client.
 func NewClient() *Client {
-	client := &Client{}
+	client := &Client{
+		Host: "",
+		Port: DefaultGrpcPort,
+		Conn: nil,
+	}
 	return client
+}
+
+// SetPort sets a port number.
+func (client *Client) SetPort(port int) {
+	client.Port = port
+}
+
+// SetHost sets a host name.
+func (client *Client) SetHost(host string) {
+	client.Host = host
+}
+
+// Open opens a gRPC connection.
+func (client *Client) Open(host string) error {
+	addr := net.JoinHostPort(client.Host, strconv.Itoa(client.Port))
+	conn, err := grpc.Dial(addr)
+	if err != nil {
+		return err
+	}
+	client.Conn = conn
+	return nil
 }
 
 func (client *Client) Execute(args []string) error {
