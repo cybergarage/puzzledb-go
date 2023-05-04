c6e9ecfded8269a439da97220d6747a6d5f65892
Satoshi Konno <skonno@cybergarage.org>
2023-05-04 23:44:36 +0900

    Add cmd.GetRootCommand()

--- puzzledb/cmd/root.go
+++ puzzledb/cmd/root.go
@@ -37,6 +37,11 @@ var rootCmd = &cobra.Command{ // nolint:exhaustruct
 	Long:  "",
 }
 
+// GetRootCommand returns the root command.
+func GetRootCommand() *cobra.Command {
+	return rootCmd
+}
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 // nolint:forbidigo
