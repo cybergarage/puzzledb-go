14e7e5a0bf5da9aa57d0e375b07a98b7948bbe0f
Satoshi Konno <skonno@cybergarage.org>
2023-05-06 19:44:35 +0900

    Add gRPC metric service

--- puzzledb/grpc.go
+++ puzzledb/grpc.go
@@ -16,12 +16,18 @@ package puzzledb
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"strconv"
+	"strings"
 
 	"github.com/cybergarage/go-logger/log"
+	pc "github.com/cybergarage/puzzledb-go/puzzledb/context"
 	pb "github.com/cybergarage/puzzledb-go/puzzledb/proto/grpc"
+	"github.com/prometheus/client_golang/prometheus"
 	"google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 )
 
 const (
@@ -37,6 +43,7 @@ type GrpcServer struct {
 	pb.UnimplementedStoreServer
 	pb.UnimplementedConfigServer
 	pb.UnimplementedHealthServer
+	pb.UnimplementedMetricServer
 }
 
 // NewGrpcServerWith returns a new GrpcServer.
@@ -49,6 +56,7 @@ func NewGrpcServerWith(server *Server) *GrpcServer {
 		UnimplementedStoreServer:  pb.UnimplementedStoreServer{},
 		UnimplementedConfigServer: pb.UnimplementedConfigServer{},
 		UnimplementedHealthServer: pb.UnimplementedHealthServer{},
+		UnimplementedMetricServer: pb.UnimplementedMetricServer{},
 	}
 }
 
@@ -79,6 +87,7 @@ func (server *GrpcServer) Start() error {
 	pb.RegisterStoreServer(server.grpcServer, server)
 	pb.RegisterConfigServer(server.grpcServer, server)
 	pb.RegisterHealthServer(server.grpcServer, server)
+	pb.RegisterMetricServer(server.grpcServer, server)
 	go func() {
 		if err := server.grpcServer.Serve(listener); err != nil {
 			log.Error(err)
@@ -119,4 +128,108 @@ func loggingUnaryInterceptor(ctx context.Context, req any, info *grpc.UnaryServe
 	}
 
 	return resp, err
+}
+
+func (server *GrpcServer) Check(context.Context, *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
+	res := pb.HealthCheckResponse{} //nolint:exhaustruct
+	res.Status = pb.HealthCheckResponse_SERVING
+	return &res, nil
+}
+
+func (server *GrpcServer) ListConfig(context.Context, *pb.ListConfigRequest) (*pb.ListConfigResponse, error) {
+	res := pb.ListConfigResponse{} //nolint:exhaustruct
+	res.Values = strings.Split(server.Config.String(), "\n")
+	return &res, nil
+}
+
+func (server *GrpcServer) GetConfig(ctx context.Context, req *pb.GetConfigRequest) (*pb.GetConfigResponse, error) {
+	v, err := server.Config.Get(req.Name)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("%s not found", req.Name))
+	}
+	res := pb.GetConfigResponse{} //nolint:exhaustruct
+	res.Value = fmt.Sprintf("%v", v)
+	return &res, nil
+}
+
+func (server *GrpcServer) GetVersion(context.Context, *pb.GetVersionRequest) (*pb.GetVersionResponse, error) {
+	res := pb.GetVersionResponse{} //nolint:exhaustruct
+	res.Value = fmt.Sprintf("%v", Version)
+	return &res, nil
+}
+
+func (server *GrpcServer) ListMetric(context.Context, *pb.ListMetricRequest) (*pb.ListMetricResponse, error) {
+	res := pb.ListMetricResponse{} //nolint:exhaustruct
+	metrics, err := prometheus.DefaultGatherer.Gather()
+	if err != nil {
+		return &res, err
+	}
+	for _, metric := range metrics {
+		res.Values = append(res.Values, metric.String())
+	}
+	res.Values = strings.Split(server.Config.String(), "\n")
+	return &res, nil
+}
+
+func (server *GrpcServer) GetMetric(ctx context.Context, req *pb.GetMetricRequest) (*pb.GetMetricResponse, error) {
+	res := pb.GetMetricResponse{} //nolint:exhaustruct
+	metrics, err := prometheus.DefaultGatherer.Gather()
+	if err != nil {
+		return &res, err
+	}
+	for _, metric := range metrics {
+		if metric.GetName() != req.Name {
+			res.Value = metric.String()
+			return &res, nil
+		}
+	}
+	return nil, status.Errorf(codes.NotFound, fmt.Sprintf("%s not found", req.Name))
+}
+
+func (server *GrpcServer) CreateDatabase(ctx context.Context, req *pb.CreateDatabaseRequest) (*pb.StatusResponse, error) {
+	res := pb.StatusResponse{} //nolint:exhaustruct
+	defaultStore, err := server.DefaultStoreService()
+	if err != nil {
+		return &res, err
+	}
+	err = defaultStore.CreateDatabase(pc.NewContext(), req.DatabaseName)
+	if err != nil {
+		return &res, err
+	}
+	return &res, nil
+}
+
+func (server *GrpcServer) RemoveDatabase(ctx context.Context, req *pb.RemoveDatabaseRequest) (*pb.StatusResponse, error) {
+	res := pb.StatusResponse{} //nolint:exhaustruct
+	defaultStore, err := server.DefaultStoreService()
+	if err != nil {
+		return &res, err
+	}
+	err = defaultStore.RemoveDatabase(pc.NewContext(), req.DatabaseName)
+	if err != nil {
+		return &res, err
+	}
+	return &res, nil
+}
+
+func (server *GrpcServer) ListDatabases(context.Context, *pb.ListDatabasesRequest) (*pb.ListDatabasesResponse, error) {
+	res := pb.ListDatabasesResponse{} //nolint:exhaustruct
+	defaultStore, err := server.DefaultStoreService()
+	if err != nil {
+		return &res, err
+	}
+	dbs, err := defaultStore.ListDatabases(pc.NewContext())
+	if err != nil {
+		return &res, err
+	}
+	res.Databases = []string{}
+	for _, db := range dbs {
+		res.Databases = append(res.Databases, db.Name())
+	}
+	return &res, nil
+}
+
+func (server *GrpcServer) ListCollections(context.Context, *pb.ListCollectionsRequest) (*pb.ListCollectionsResponse, error) {
+	res := pb.ListCollectionsResponse{} //nolint:exhaustruct
+	return &res, nil
 }
