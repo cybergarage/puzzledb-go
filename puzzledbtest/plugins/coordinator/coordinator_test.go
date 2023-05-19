f12a0f45f3b2d17b0470ad85afcc80632cae6eee
Satoshi Konno <skonno@cybergarage.org>
2023-05-19 23:39:06 +0900

    Update TestCoordinator()

--- puzzledbtest/plugins/coordinator/coordinator_test.go
+++ puzzledbtest/plugins/coordinator/coordinator_test.go
@@ -38,10 +38,10 @@ func TestCoordinator(t *testing.T) {
 					}
 				}()
 				t.Run("message", func(t *testing.T) {
-					// CoordinatorMessageTest(t, coord)
+					CoordinatorMessageTest(t, coord)
 				})
 				t.Run("process", func(t *testing.T) {
-					CoordinatorProcessTest(t, coord)
+					// CoordinatorProcessTest(t, coord)
 				})
 			})
 		}
