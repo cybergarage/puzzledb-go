3e22e0dfdca653f8a19dfe2e7ea81a7a449578a9
Satoshi Konno <skonno@cybergarage.org>
2023-05-31 18:02:35 +0900

    Update query.BaseService::SetStore() to register the cache key prefix for the database and collection

--- puzzledb/plugins/query/service.go
+++ puzzledb/plugins/query/service.go
@@ -17,7 +17,10 @@ package query
 import (
 	"github.com/cybergarage/go-tracing/tracer"
 	"github.com/cybergarage/puzzledb-go/puzzledb/coordinator"
+	"github.com/cybergarage/puzzledb-go/puzzledb/document/kv"
 	"github.com/cybergarage/puzzledb-go/puzzledb/plugins"
+	docStore "github.com/cybergarage/puzzledb-go/puzzledb/plugins/store"
+	"github.com/cybergarage/puzzledb-go/puzzledb/plugins/store/kvcache"
 	"github.com/cybergarage/puzzledb-go/puzzledb/store"
 )
 
@@ -72,6 +75,19 @@ func (service *BaseService) Coordinator() coordinator.Coordinator {
 // SetStore sets the store.
 func (service *BaseService) SetStore(store store.Store) {
 	service.store = store
+
+	// Register the cache key prefix for the database and collection.
+	kvDocStore, ok := store.(docStore.DocumentKvStore)
+	if !ok {
+		return
+	}
+	kvStore := kvDocStore.KvStore()
+	cacheStore, ok := kvStore.(kvcache.CacheStore)
+	if !ok {
+		return
+	}
+	cacheStore.RegisterCacheKeyPrefix(kv.DatabaseKeyHeader)
+	cacheStore.RegisterCacheKeyPrefix(kv.CollectionKeyHeader)
 }
 
 // Store returns the store.
