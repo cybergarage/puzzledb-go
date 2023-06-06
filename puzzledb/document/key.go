9594042fa1b2b428180e4e128521af4561c6e19a
Satoshi Konno <skonno@cybergarage.org>
2023-06-06 23:28:11 +0900

    Add document.Key::Collection()

--- puzzledb/document/key.go
+++ puzzledb/document/key.go
@@ -55,6 +55,18 @@ func (key Key) Database() (string, error) {
 	return v, nil
 }
 
+// Collection returns the collection name of the key.
+func (key Key) Collection() (string, error) {
+	if key.Len() < 2 {
+		return "", newCollectionKeyNotFoundError(key)
+	}
+	v, ok := key[1].(string)
+	if !ok {
+		return "", newCollectionKeyNotFoundError(key)
+	}
+	return v, nil
+}
+
 // Equals returns true if the specified key is equal to the key.
 func (key Key) Equals(other Key) bool {
 	if len(key) != len(other) {
