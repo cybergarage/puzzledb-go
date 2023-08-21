3baa720ef83e906e577997ed9ae0fd519277f1a0
Satoshi Konno <skonno@cybergarage.org>
2023-08-21 20:26:32 +0900

    Update postgresql.NewService()

---
+++ puzzledb/plugins/query/postgresql/executor.go
@@ -0,0 +1,66 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package postgresql
+
+import (
+	"github.com/cybergarage/go-postgresql/postgresql"
+	"github.com/cybergarage/go-postgresql/postgresql/protocol/message"
+	"github.com/cybergarage/go-postgresql/postgresql/query"
+)
+
+// CreateDatabase handles a CREATE DATABASE query.
+func (service *Service) CreateDatabase(*postgresql.Conn, *query.CreateDatabase) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("CREATE DATABASE")
+}
+
+// CreateTable handles a CREATE TABLE query.
+func (service *Service) CreateTable(*postgresql.Conn, *query.CreateTable) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("CREATE TABLE")
+}
+
+// CreateIndex handles a CREATE INDEX query.
+func (service *Service) CreateIndex(*postgresql.Conn, *query.CreateIndex) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("CREATE INDEX")
+}
+
+// DropDatabase handles a DROP DATABASE query.
+func (service *Service) DropDatabase(*postgresql.Conn, *query.DropDatabase) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("DROP DATABASE")
+}
+
+// DropIndex handles a DROP INDEX query.
+func (service *Service) DropTable(*postgresql.Conn, *query.DropTable) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("DROP TABLE")
+}
+
+// Insert handles a INSERT query.
+func (service *Service) Insert(*postgresql.Conn, *query.Insert) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("INSERT")
+}
+
+// Select handles a SELECT query.
+func (service *Service) Select(*postgresql.Conn, *query.Select) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("SELECT")
+}
+
+// Update handles a UPDATE query.
+func (service *Service) Update(*postgresql.Conn, *query.Update) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("UPDATE")
+}
+
+// Delete handles a DELETE query.
+func (service *Service) Delete(*postgresql.Conn, *query.Delete) (message.Responses, error) {
+	return nil, postgresql.NewErrNotImplemented("DELETE")
+}
