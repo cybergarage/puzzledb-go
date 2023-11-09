4f6bd399e10318c1b78ab8811e1e42bb6b9421da
Satoshi Konno <skonno@cybergarage.org>
2023-11-10 00:38:44 +0900

    Add TestCacheStores()

---
+++ puzzledbtest/plugins/store/kvcache/store_test.go
@@ -0,0 +1,40 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kvcache
+
+import (
+	"testing"
+
+	"github.com/cybergarage/puzzledb-go/puzzledbtest"
+)
+
+//nolint:gosec,cyclop,gocognit,gocyclo,maintidx
+func TestCacheStores(t *testing.T) {
+	t.Helper()
+
+	mgr := puzzledbtest.NewPluginManager()
+	for _, kvStore := range mgr.EnabledKvStoreServices() {
+		for _, keyCoder := range mgr.EnabledKeyCoderServices() {
+			kvStore.SetKeyCoder(keyCoder)
+			if err := kvStore.Start(); err != nil {
+				t.Error(err)
+				return
+			}
+			if err := kvStore.Stop(); err != nil {
+				t.Error(err)
+			}
+		}
+	}
+}
