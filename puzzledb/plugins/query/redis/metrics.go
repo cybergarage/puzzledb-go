20b530696ea5dfca3ed4917f265b6beff3b1e747
Satoshi Konno <skonno@cybergarage.org>
2023-12-26 12:26:19 +0900

    Add hash command metrics

--- puzzledb/plugins/query/redis/metrics.go
+++ puzzledb/plugins/query/redis/metrics.go
@@ -22,17 +22,37 @@ var (
 	//nolint:exhaustruct
 	mSetLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name: "redis_set_latency",
-		Help: "redis set latency in milliseconds",
+		Help: "redis SET latency in milliseconds",
 	})
 	//nolint:exhaustruct
 	mGetLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name: "redis_get_latency",
-		Help: "redis get latency in milliseconds",
+		Help: "redis GET latency in milliseconds",
 	})
 	//nolint:exhaustruct
 	mDelLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name: "redis_del_latency",
-		Help: "redis del latency in milliseconds",
+		Help: "redis DEL latency in milliseconds",
+	})
+	//nolint:exhaustruct
+	mHSetLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "redis_hset_latency",
+		Help: "redis HSET latency in milliseconds",
+	})
+	//nolint:exhaustruct
+	mHGetLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "redis_hget_latency",
+		Help: "redis HGET latency in milliseconds",
+	})
+	//nolint:exhaustruct
+	mHGetAllLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "redis_hgetall_latency",
+		Help: "redis HGETALL latency in milliseconds",
+	})
+	//nolint:exhaustruct
+	mHDelLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "redis_hdel_latency",
+		Help: "redis HDEL latency in milliseconds",
 	})
 )
 
@@ -40,4 +60,8 @@ func init() { //nolint:gochecknoinits
 	prometheus.MustRegister(mSetLatency)
 	prometheus.MustRegister(mGetLatency)
 	prometheus.MustRegister(mDelLatency)
+	prometheus.MustRegister(mHSetLatency)
+	prometheus.MustRegister(mHGetLatency)
+	prometheus.MustRegister(mHGetAllLatency)
+	prometheus.MustRegister(mHDelLatency)
 }
