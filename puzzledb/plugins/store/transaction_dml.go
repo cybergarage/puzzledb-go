015e034532a282a9c3b25bc08a504f46c31aff93
Satoshi Konno <skonno@cybergarage.org>
2024-12-06 21:15:21 +0900

    Add DatabaseOperation::ListCollections()

--- puzzledb/plugins/store/transaction_dml.go
+++ puzzledb/plugins/store/transaction_dml.go
@@ -42,7 +42,33 @@ func (txn *transaction) setCollection(ctx context.Context, col store.Collection)
 	return txn.kv.Set(&kvObj)
 }
 
-// CreateCollection creates a new schema.
+// ListCollections returns the all collection in the database.
+func (txn *transaction) ListCollections(ctx context.Context) ([]store.Collection, error) {
+	ctx.StartSpan("ListCollections")
+	defer ctx.FinishSpan()
+
+	kvSchemaKey := kv.NewKeyWith(kv.CollectionKeyHeader, document.NewKeyWith(txn.Database().Name()))
+	kvRs, err := txn.kv.GetRange(kvSchemaKey)
+	if err != nil {
+		return nil, err
+	}
+	cols := make([]store.Collection, 0)
+	for kvRs.Next() {
+		kvObj := kvRs.Object()
+		obj, err := txn.DecodeDocument(bytes.NewReader(kvObj.Value))
+		if err != nil {
+			return nil, err
+		}
+		col, err := document.NewCollectionWith(obj)
+		if err != nil {
+			return nil, err
+		}
+		cols = append(cols, col)
+	}
+	return cols, nil
+}
+
+// CreateCollection creates a new collection in into database.
 func (txn *transaction) CreateCollection(ctx context.Context, col store.Collection) error {
 	ctx.StartSpan("CreateCollection")
 	defer ctx.FinishSpan()
@@ -58,7 +84,7 @@ func (txn *transaction) UpdateCollection(ctx context.Context, col store.Collecti
 	return txn.setCollection(ctx, col)
 }
 
-// GetCollection returns the specified schema.
+// GetCollection returns the specified collection in the database.
 func (txn *transaction) GetCollection(ctx context.Context, name string) (store.Collection, error) {
 	ctx.StartSpan("GetCollection")
 	defer ctx.FinishSpan()
@@ -79,7 +105,7 @@ func (txn *transaction) GetCollection(ctx context.Context, name string) (store.C
 	return document.NewCollectionWith(obj)
 }
 
-// RemoveCollection removes the specified schema.
+// RemoveCollection removes the specified collection in the database.
 func (txn *transaction) RemoveCollection(ctx context.Context, name string) error {
 	ctx.StartSpan("RemoveCollection")
 	defer ctx.FinishSpan()
@@ -88,7 +114,7 @@ func (txn *transaction) RemoveCollection(ctx context.Context, name string) error
 	return txn.kv.Remove(kvSchemaKey)
 }
 
-// TruncateCollections removes all schemas.
+// TruncateCollections removes all collections in the database.
 func (txn *transaction) TruncateCollections(ctx context.Context) error {
 	ctx.StartSpan("TruncateCollections")
 	defer ctx.FinishSpan()
