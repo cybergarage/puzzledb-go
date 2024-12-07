c81358532263fcaa5dafee1d064812483744b2da
Satoshi Konno <skonno@cybergarage.org>
2024-12-07 19:22:13 +0900

    Add coordinator.ReadAll()

---
+++ puzzledb/coordinator/util.go
@@ -0,0 +1,24 @@
+// Copyright (C) 2024 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package coordinator
+
+// ReadAll reads all objects from the result set.
+func ReadAll(rs ResultSet) ([]Object, error) {
+	objs := []Object{}
+	for rs.Next() {
+		objs = append(objs, rs.Object())
+	}
+	return objs, nil
+}
