81e4eddff4921f1f39ddf7db5b6570cc063b3785
Satoshi Konno <skonno@cybergarage.org>
2024-12-08 17:58:28 +0900

    Update util.Store::DumpCollection()

--- puzzledb/util/store.go
+++ puzzledb/util/store.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 The PuzzleDB Authors.
+// Copyright (C) 2024 The PuzzleDB Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,94 +12,109 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package util
+package document
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
 
-	dockv "github.com/cybergarage/puzzledb-go/puzzledb/document/kv"
-	"github.com/cybergarage/puzzledb-go/puzzledb/plugins/store"
-	"github.com/cybergarage/puzzledb-go/puzzledb/store/kv"
+	"github.com/cybergarage/puzzledb-go/puzzledb/context"
+	"github.com/cybergarage/puzzledb-go/puzzledb/document"
+	plugin "github.com/cybergarage/puzzledb-go/puzzledb/plugins/store"
+	"github.com/cybergarage/puzzledb-go/puzzledb/store"
 )
 
-// Store represents a document store.
+// Store represents a new document store utility instance.
 type Store struct {
-	*store.Store
+	store        plugin.DocumentStore
+	dumpChildren bool
 }
 
-// NewStoreWith creates a new store with the given object.
-func NewStoreWith(obj any) (*Store, error) {
-	store, ok := obj.(*store.Store)
-	if !ok {
-		return nil, errors.New("failed to get kv store")
+// StoreOption represents a store option.
+type StoreOption func(*Store)
+
+func WithStoreDumpChildren(v bool) StoreOption {
+	return func(s *Store) {
+		s.dumpChildren = v
 	}
-	return &Store{
-		Store: store,
-	}, nil
 }
 
-// Dump returns a string representation of the store.
+// NewStoreWith returns a new document store utility instance with the specified store.
+func NewStoreWith(docStore plugin.DocumentStore, opts ...StoreOption) *Store {
+	store := &Store{
+		store:        docStore,
+		dumpChildren: true,
+	}
+	for _, opt := range opts {
+		opt(store)
+	}
+	return store
+}
+
+// Dump returns a string array representation of the document store.
 func (store *Store) Dump() ([]string, error) {
-	lines := []string{}
-
-	docStore := store.Store
-	kvStore := docStore.KvStore()
-
-	tx, err := kvStore.Transact(false)
+	ctx := context.NewContext()
+	allLines := []string{}
+	dbs, err := store.store.ListDatabases(ctx)
 	if err != nil {
-		return lines, nil
+		return allLines, err
 	}
-
-	keys := []kv.Key{
-		kv.NewKeyWith(kv.DatabaseKeyHeader, kv.Key{}),
-		kv.NewKeyWith(kv.CollectionKeyHeader, kv.Key{}),
-		kv.NewKeyWith(kv.PrimaryIndexHeader, kv.Key{}),
-		kv.NewKeyWith(kv.SecondaryIndexHeader, kv.Key{}),
-		kv.NewKeyWith(kv.DocumentKeyHeader, kv.Key{}),
-	}
-
-	for _, key := range keys {
-		rs, err := tx.GetRange(key)
-		if err != nil {
+	for n, db := range dbs {
+		allLines = append(allLines, fmt.Sprintf("[%d]: %s", n, db.Name()))
+		if !store.dumpChildren {
 			continue
 		}
-		for rs.Next() {
-			obj, err := rs.Object()
-			if err != nil {
-				continue
-			}
-			keys := obj.Key().Elements()
-			keyHeaderBytes, ok := keys[0].([]byte)
-			if !ok {
-				lines = append(lines, fmt.Sprintf("%v: %v", keys[1:], obj.Value()))
-			}
-			keyHeader := dockv.NewKeyHeaderFrom(keyHeaderBytes)
-
-			switch keyHeader.Type() {
-			case dockv.DatabaseObject, dockv.CollectionObject, dockv.DocumentObject:
-				r := bytes.NewReader(obj.Value())
-				val, err := docStore.DecodeDocument(r)
-				if err != nil {
-					lines = append(lines, fmt.Sprintf("%v %v: %v", keyHeader, keys[1:], obj.Value()))
-					continue
-				}
-				lines = append(lines, fmt.Sprintf("%v %v: %v", keyHeader, keys[1:], val))
-			case dockv.IndexObject:
-				idxKeys, err := docStore.DecodeKey(obj.Value())
-				if err != nil {
-					lines = append(lines, fmt.Sprintf("%v %v: %v", keyHeader, keys[1:], obj.Value()))
-					continue
-				}
-				idxKeyHederBytes, ok := idxKeys[0].([]byte)
-				if !ok {
-					lines = append(lines, fmt.Sprintf("%v %v: %v", keyHeader, keys[1:], idxKeys))
-				}
-				idxKeyHeder := dockv.NewKeyHeaderFrom(idxKeyHederBytes)
-				lines = append(lines, fmt.Sprintf("%v %v: %v %v", keyHeader, keys[1:], idxKeyHeder, idxKeys[1:]))
-			}
+		lines, err := store.DumpDatabase(ctx, db)
+		if err != nil {
+			return allLines, err
 		}
+		allLines = append(allLines, lines...)
 	}
-	return lines, tx.Commit()
+	return allLines, nil
+}
+
+// DumpDatabase returns a string array representation of the specified database.
+func (store *Store) DumpDatabase(ctx context.Context, db store.Database) ([]string, error) {
+	allLines := []string{}
+	txn, err := db.Transact(false)
+	if err != nil {
+		return allLines, err
+	}
+
+	defer func() {
+		txn.Commit(ctx)
+	}()
+
+	cols, err := txn.ListCollections(ctx)
+	if err != nil {
+		return allLines, err
+	}
+	for n, col := range cols {
+		allLines = append(allLines, fmt.Sprintf("[%d]: %s", n, col.Name()))
+		if !store.dumpChildren {
+			continue
+		}
+		lines, err := store.DumpCollection(ctx, db, col, txn)
+		if err != nil {
+			return allLines, err
+		}
+		allLines = append(allLines, lines...)
+	}
+	return allLines, nil
+}
+
+func (store *Store) DumpCollection(ctx context.Context, db store.Database, col store.Collection, txn store.Transaction) ([]string, error) {
+	allLines := []string{}
+	key := document.NewKeyWith(db.Name(), col.Name())
+	rs, err := txn.FindObjects(ctx, key)
+	if err != nil {
+		return allLines, err
+	}
+	for rs.Next() {
+		doc, err := rs.Document()
+		if err != nil {
+			return allLines, err
+		}
+		allLines = append(allLines, fmt.Sprintf("[%s]: %s", doc.Key(), doc.Object()))
+	}
+	return allLines, nil
 }
