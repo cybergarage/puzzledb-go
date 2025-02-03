9d0ff59fb2c83e4e62e8ef20214ce0680eb190e6
Satoshi Konno <skonno@cybergarage.org>
2025-02-03 21:06:57 +0900

    Update TestPostgreSQLTestSuite()

--- puzzledbtest/plugins/query/postgresql/service_suite_test.go
+++ puzzledbtest/plugins/query/postgresql/service_suite_test.go
@@ -43,7 +43,7 @@ func TestPostgreSQLTestSuite(t *testing.T) {
 	}
 
 	var databaseDump string
-	dumpDatabase := func(*sqltest.Suite, *sqltest.ScenarioRunner, error) {
+	dumpDatabase := func(*sqltest.Suite, *sqltest.ScenarioTester, error) {
 		databaseDump = server.Store().String()
 	}
 
